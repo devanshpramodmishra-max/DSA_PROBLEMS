b0c7d5c169dd9c252e499a513d7849ae20b6cdc0
Devansh_Mishra <devanshpramodmishra@gmail.com>
2026-09-01 13:03:53 +0530

    Time: 0 ms (100%), Space: 48.5 MB (11.13%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public double solve(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        double half=solve(x,n/2);
+        if(n%2==0){return half*half;}
+        else{return x*half*half;}
+    }
+
+    public double myPow(double x, int n) {
+
+        long N = n;
+
+        if (N < 0) {
+            return 1 / solve(x, -N);
+        }
+
+        return solve(x, N);
+    }
+}
